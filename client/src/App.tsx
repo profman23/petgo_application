5c8cf3f59490870ec213e7b43b70300d98311b22
mohamedghazal3 <42621288-mohamedghazal3@users.noreply.replit.com>
2025-08-30 12:16:12 +0000

    Restored to '2c6d0b412255c27213643d5d85c41bf85efbbc1a'
    Replit-Restored-To: 2c6d0b412255c27213643d5d85c41bf85efbbc1a

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -24,7 +24,6 @@ import AdminLogin from "@/pages/admin-login";
 import AdminDashboard from "@/pages/admin-dashboard";
 import AdminServices from "@/pages/admin-services";
 import AdminProducts from "@/pages/admin-products";
-import AdminImport from "@/pages/admin-import";
 import AdministrationUsers from "@/pages/administration-users";
 import AdministrationAuthorization from "@/pages/administration-authorization";
 import SalesReports from "@/pages/sales-reports";
@@ -147,7 +146,6 @@ function Router() {
           <Route path="/admin-dashboard" component={AdminDashboard} />
           <Route path="/admin-dashboard/services" component={AdminServices} />
           <Route path="/admin-dashboard/products" component={AdminProducts} />
-          <Route path="/admin-dashboard/import" component={AdminImport} />
           <Route path="/administration/users" component={AdministrationUsers} />
           <Route path="/administration/authorization" component={AdministrationAuthorization} />
           <Route path="/sales-reports" component={SalesReports} />
